198a73b365a16bc91e02176737dc0b805278711e
Mugundhan G <mugundhan10@gmail.com>
2024-11-27 13:13:10 +0530

    Created basic spring program

---
+++ src/main/java/dev/mugi/scaler/firstspringprojectscaler/FirstSpringProjectScalerApplication.java
@@ -0,0 +1,13 @@
+package dev.mugi.scaler.firstspringprojectscaler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringProjectScalerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringProjectScalerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/mugi/scaler/firstspringprojectscaler/FirstSpringProjectScalerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mugi.scaler.firstspringprojectscaler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringProjectScalerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
