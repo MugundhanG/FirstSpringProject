9ae8fd68ce2e1a7e33a78f222a6ceaa763dd1a1c
Mugundhan G <mugundhan10@gmail.com>
2024-11-29 14:41:45 +0530

    Using Path variable

--- src/main/java/dev/mugi/scaler/firstspringprojectscaler/Hello.java
+++ src/main/java/dev/mugi/scaler/firstspringprojectscaler/Hello.java
@@ -1,5 +1,6 @@
 package dev.mugi.scaler.firstspringprojectscaler;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +10,11 @@ public class Hello {
     @RequestMapping("hello")
     public String sayHello() {
         return "Hello, Developer!";
+    }
+
+    @RequestMapping("hello/{name}")
+    public String sayHelloWhenLogin(@PathVariable("name") String userName) {
+        return "Hello, " + userName + "!";
     }
 
 }
